5a3f3ad67064b664a3fd2052dda5425b6ea96c44
Dan Svoboda <dan@dansvo.com>
2020-08-02 18:12:41 -0400

    modify release.nix to have who separate attibutes: one which produces the compiled-haskell, chart-generating program, and one which prodices the star chart itself

--- release.nix
+++ release.nix
@@ -1,4 +1,19 @@
 let
     pkgs = import <nixpkgs> { };
+    star-data = import ./star-data.nix {};
+    stars = star-data.hyg-star-data;
+    lines = star-data.constellation-lines;
 in
-    pkgs.haskellPackages.callPackage ./default.nix { }
+  rec {
+    program = pkgs.haskellPackages.callPackage ./default.nix { };
+    chart = pkgs.stdenv.mkDerivation {
+      name = "star-chart";
+      program = program;
+      stars = stars;
+      lines = lines;
+      buildCommand = ''
+        declare -xp
+        $program/bin/star-chart $stars $lines $out
+      '';
+    };
+  }
