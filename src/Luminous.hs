7ed94ba4c93013a81a0611135db8102cb04fcf2d
Dan Svoboda <dan@dansvo.com>
2020-08-06 11:40:38 -0400

    move visual magnitude into separate module in preparation for introducing a Luminous typeclass to handle the fact that both stars and star systems can have apparent visual magnitude

---
+++ src/Luminous.hs
@@ -0,0 +1,18 @@
+module Luminous where
+
+type VisualMagnitude = Double
+
+----------------
+-- functions to add visual magnitudes of stars
+----------------
+-- this method for adding star magnitudes taken from the following forum post:
+-- www.physicsforum.com/threads/apparent-magnitude-of-two-stars.155292
+vmag_to_lum :: Floating a => a -> a
+vmag_to_lum vmag = 10 ** (vmag / (-2.5))
+
+lum_to_vmag :: Floating a => a -> a
+lum_to_vmag lum = (-2.5) * (logBase 10 lum)
+
+add_vmags :: (Functor f, Foldable f, Floating a) => f a -> a
+add_vmags  = lum_to_vmag . sum . (fmap vmag_to_lum)
+
--- src/Stars/Types.hs
+++ src/Stars/Types.hs
@@ -2,6 +2,7 @@ module Stars.Types where
 
 import Data.List (minimumBy)
 import Location
+import Luminous
 
 type HipparcosNumber = Int
 type Constellation = String
@@ -11,34 +12,21 @@ data Star = Star
     , constellation   :: Constellation
     , bayer  :: String    -- Bayer designation
     , properName   :: String    -- Proper name, like "Sirius"
-    , vmag   :: Double    -- visual magnitude
+    , vmag   :: VisualMagnitude
     , location   :: Location
     } deriving Show
 
 instance Eq Star where
     (==) star1 star2 = (hipparcosNumber star1) == (hipparcosNumber star2)
 
-----------------
--- functions to add visual magnitudes of stars
-----------------
--- this method for adding star magnitudes taken from the following forum post:
--- www.physicsforum.com/threads/apparent-magnitude-of-two-stars.155292
-vmag_to_lum :: Floating a => a -> a
-vmag_to_lum vmag = 10 ** (vmag / (-2.5))
-
-lum_to_vmag :: Floating a => a -> a
-lum_to_vmag lum = (-2.5) * (logBase 10 lum)
-
-add_vmags :: (Functor f, Foldable f, Floating a) => f a -> a
-add_vmags  = lum_to_vmag . sum . (fmap vmag_to_lum)
-
-total_vmag :: [Star] -> Double
-total_vmag = add_vmags . (fmap vmag)
-
-brightest :: [Star] -> Star
-brightest = minimumBy (\s1 s2 -> compare (vmag s1) (vmag s2))
-
 data ConstLine = ConstLine
     { pt1 :: String
     , pt2 :: String
     } deriving Show
+
+brightest :: [Star] -> Star
+brightest = minimumBy (\s1 s2 -> compare (vmag s1) (vmag s2))
+
+total_vmag :: [Star] -> Double
+total_vmag = Luminous.add_vmags . (fmap vmag)
+
