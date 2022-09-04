37ccbd90f1d9aa092d484c902555597b712f0483
Parallels <parallels@debian-gnu-linux-10.localdomain>
2022-09-04 10:11:08 -0400

    add command line handling with optparse-applicative
    the command line options for this program are about to get more complex
    as I add support for locations and times. In preparation for this, I
    make the command line option handling more robust/extensible here.

--- src/Stars.hs
+++ src/Stars.hs
@@ -12,6 +12,7 @@ import Location
 import Luminous (brighterThan)
 import ApparentStar
 import Data.List.NonEmpty hiding ((!!))
+import Options.Applicative
 
 -- functions to deal with the fact that visual binaries appear
 -- as a single star to the unaided eye
@@ -30,14 +31,34 @@ vis_list stars = foldr f [] stars
 filterStars :: [Maybe Star] -> [ApparentStar]
 filterStars xs = vis_list $ (Prelude.filter (\x -> x `brighterThan` 7.5 && properName x /= "Sol")) (catMaybes xs)
 
+data CommandLineOptions = CommandLineOptions
+    { star_file :: String
+    , constellation_file :: String
+    , out_path :: String
+    } deriving Show
+
+commandline_parser :: Parser CommandLineOptions
+commandline_parser = CommandLineOptions
+    <$> strOption
+        (  long "star_file"
+        <> help "path to csv star file" )
+    <*> strOption
+        (  long "constellation_file"
+        <> help "path to constellation line file" )
+    <*> strOption
+        (  long "out_path"
+        <> help "path to file that should be produced" )
+
+parser_info = info commandline_parser (header "TEST")
+
 main :: IO ()
 main = do
-    args <- getArgs
+    options <- execParser parser_info
     putStrLn "parsing star file"
-    ethStars <- (fmap.fmap) filterStars $ parseStarFile <$> readFile ( args !! 0 )
+    ethStars <- (fmap.fmap) filterStars $ parseStarFile <$> readFile ( star_file options )
     putStrLn "parsing constellation file"
-    ethConstLines <- parseCLFile <$> readFile ( args !! 1 )
-    let outPath = args !! 2
+    ethConstLines <- parseCLFile <$> readFile ( constellation_file options )
+    let outPath = out_path options
     putStrLn $ "rendering"
     sequenceA $ make_svg <$> ethStars <*> pure [] <*> pure outPath -- ethConstLines
     return ()
